7ba102a4cc2dd33f67c558b720296b61301fd804
davien21 <chidiebereekennia@gmail.com>
2021-12-18 08:29:50 +0100

    --refactor: fix ux bug on loader

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const BeimaContractAddress = '0x097F84B9926e397765Df4B191310961eA4fd9729';
+const BeimaContractAddress = '0xAB524Fc6F9c00c2d2D755CD394d03b4BF7bfC75a';
 const RinkebyUSDTContractAddress = '0xD9BA894E0097f8cC2BBc9D24D308b98e36dc6D02';
 
 export { BeimaContractAddress, RinkebyUSDTContractAddress };
